c087056e3ab83704f722e8007e951b79dd66bb21
juno <xrit412@gmail.com>
2023-10-11 18:16:15 +0900

    exam.js 변경

--- src/exam.js
+++ src/exam.js
@@ -24,13 +24,25 @@ $(document).ready(function() {
 	$('body').append('<span></span>');
 	$('body').append('<hr>');
 	
-	for(let i=0; i<10; i++)
+	for(let i=0; i<50; i++)
 		$('body').append('<img alt="xxx">');
 		
 	$('button').click(function(e) {
 		$('img')
-				.attr('src', 'http://via.placeholder.com/100X100')
+				.attr('src', function(i) {
+					let w = parseInt(Math.random()*10 * 10);
+					let h = parseInt(Math.random()*10 * 10);
+					return `http://via.placeholder.com/${w}X${h}`
+				}) 
 				.css('margin', 10)
+	});
+	
+	$('img').on('load', function() {
+//		$(this).fadeOut(0)
+//			   .fadeIn(3000);		// 이미지 트랜지션 줄때
+
+		$(this).slideUp(0)
+			   .slideDown(3000);
 	});
 	
 	time();
